d7035481a93330e6da5f4c453342e2d442f1eb40
WenzelburgerMarc <54157548+WenzelburgerMarc@users.noreply.github.com>
2023-05-23 16:06:34 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'grievy',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
